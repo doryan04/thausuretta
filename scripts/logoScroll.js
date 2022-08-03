1133eaff2a4d4c4534f1fb5367d98fb6b80e75a7
ItzAdel <gdquasar@bk.ru>
2022-08-03 20:42:22 +0400

    HTML fix

--- scripts/logoScroll.js
+++ scripts/logoScroll.js
@@ -3,6 +3,7 @@ addEventListener('scroll', function(){
     let koef = 2.5;
     let scroll = window.pageYOffset;
     koef = 2.5 + scroll/50;
-    logo.style.transform = "scale(calc(100% / " + koef + ")) translate(0)";
+    logo.style.transform = "scale(calc(100% / " + koef + "))";
+    logo.style.bottom = -(scroll/17) + "%";
     logo.style.opacity = "1" - scroll * 0.002;
 })
